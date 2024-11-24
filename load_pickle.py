d040cb552a66dd375b28c89d496259e84df0b713
Ansh <anshagrawal181@gmail.com>
2024-11-24 21:44:41 +0530

    first commit

---
+++ load_pickle.py
@@ -0,0 +1,7 @@
+# import pickle
+
+# # Replace 'yourfile.pkl' with the path to your .pkl file
+# with open('diabetes_prediction.pkl', 'rb') as file:
+#     data = pickle.load(file)
+
+# print(data.keys())
